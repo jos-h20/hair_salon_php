ad5bad8d50e8a13aa9be3e060685a0c24908154d
Josh Overly <joshoverly@yahoo.com>
2016-02-26 08:35:47 -0800

    add composer and create database tables, fail first spec for Stylist

---
+++ src/Stylist.php
@@ -0,0 +1,24 @@
+<?php
+    class Stylist
+    {
+        private $stylist_name;
+        private $id;
+
+        function __construct($stylist_name, $id = null)
+        {
+            $this->stylist_name = $stylist_name;
+            $this->id = $id;
+        }
+
+        // function setStylistName($new_stylist_name)
+        // {
+        //     $this->stylist_name = (string) $new_stylist_name;
+        // }
+        //
+        // function getStylistName()
+        // {
+        //     return $this->stylist_name;
+        // }
+
+    }
+?>
---
+++ tests/StylistTest.php
@@ -0,0 +1,34 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Stylist.php";
+
+    $server = 'mysql:host=localhost;dbname=hair_salon_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class StylistTest extends PHPUnit_Framework_TestCase
+    {
+
+        function test_getStylistName()
+        {
+            //Arrange
+            $type = "Indian";
+            $test_Stylist = new Stylist($stylist_name);
+
+            //Act
+            $result = $test_Stylist->getStylistName();
+
+            //Assert
+            $this->assertEquals($stylist_name, $result);
+        }
+
+
+    }
+
+?>
