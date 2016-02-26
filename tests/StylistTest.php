6a63c5531d1257427239d37a70e5376b914aca2e
Josh Overly <joshoverly@yahoo.com>
2016-02-26 08:58:30 -0800

    add find test, fail sixth spec

--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -91,6 +91,22 @@
             //Assert
             $this->assertEquals([], $result);
         }
+        function test_find()
+        {
+            //Arrange
+            $stylist_name = "Frida";
+            $stylist_name2 = "Harriet";
+            $test_stylist = new Stylist($stylist_name);
+            $test_stylist->save();
+            $test_stylist2 = new Stylist($stylist_name2);
+            $test_stylist2->save();
+
+            //Act
+            $result = Stylist::find($test_stylist->getId());
+
+            //Assert
+            $this->assertEquals($test_stylist, $result);
+        }
 
     }
 
